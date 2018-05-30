2262394613170a7821951c6c41ce8265c1019025
Romain Mueller <romain.mueller@gmail.com>
2018-05-30 12:10:41 +0200

    added book bibtex category

--- tex2bib.py
+++ tex2bib.py
@@ -1,3 +1,4 @@
+#! /bin/env python -u
 #
 # tex2bib.py
 #
@@ -11,8 +12,9 @@
 # This script is is as KISS as it can be: no crazy error reporting, no logging
 # mechanism, no nothing actually. Use it with love, still.
 
-import sys, re, time, urllib2
+import sys, re, time, urllib2, socket
 import arxiv2bib
+import argparse
 from glob import glob
 
 print "%"
@@ -22,8 +24,22 @@ print "%"
 # ------------------------------------------------------------------------------
 # Init
 
+#parser = argparse.ArgumentParser(description='Fetch bib citations from tha internet!')
+#parser.add_argument('integers', metavar='N', type=int, nargs='+',
+#                    help='an integer for the accumulator')
+#parser.add_argument('--exclude', dest='accumulate', action='store_const',
+#                    const=sum, default=max,
+#                    help='sum the integers (default: find the max)')
+#
+#args = parser.parse_args()
+#print args.accumulate(args.integers)
+
+# counting errors is what i've done all my life
+errcount = 0
+warcount = 0
+
 # recognized citation types (lowercase!)
-types = [ 'doi', 'arxiv', 'inspire' ]
+types = [ 'doi', 'arxiv', 'inspire', 'isbn' ]
 
 if len(sys.argv)==1:
     print "% Error: Please provide at least one input file"
@@ -57,28 +73,38 @@ for f in filelist:
             match = re.match(r'((.+?):(.+))', s)
             # check that format xxx:yyy is respected
             if match==None:
-                print '% Discarding citation', s
+                print '% Warning: discarding citation', s
+                warcount += 1
             else:
                 allmatches.append(match)
 
     for m in allmatches:
 
+        cit = re.sub(r'^\s', '', m.group(1))
+
         # strip whitesaces
-        t = ''.join(m.group(2).split()).lower() # + lowercase
-        i = ''.join(m.group(3).split())
+        e = re.sub(r'\s', '', cit)
+        t = re.sub(r'\s', '', m.group(2)).lower() # + lowercase
+        i = re.sub(r'\s', '', m.group(3))
+
+        # white spaces are not allowed: warn the user if we find one
+        if e!=cit:
+            print '% Warning: citation \''+ cit, '\' contains white spaces'
 
         # check that we have implemented the type
         if t not in types:
-            print '% Discarding citation', m.group(1)
+            print '% Warning: discarding citation', m.group(1)
+            warcount += 1
             continue
 
         # add to the list if not there yet
         if next((c for c in citations
-                 if c['type']==t and c['identifier']==i), 0)==0:
+                   if c['type']==t
+                   and c['identifier']==i), 0)==0:
             citations += [{
-                'entry' : m.group(1), 'type' : t, 'identifier' : i
+                'entry': e, 'type' : t, 'identifier' : i,
                 }]
-            print '% Found citation', m.group(1)
+            print '% Found citation', e
 
 print
 
@@ -93,7 +119,7 @@ print
 
 # replace the bibtex identifier with the correct one for a given entry
 def replace_id(dat, entry):
-    return re.sub(r'@(article|report|inproceedings|phdthesis){(.*?),',
+    return re.sub(r'@(article|report|inproceedings|phdthesis|book){(.*?),',
                   r'@\1{'+entry+',', dat)
 
 #
@@ -104,7 +130,13 @@ for c in [ c for c in citations if c['type']=='doi' ]:
     # fetch from crossref.org
     url = (r'http://search.crossref.org/citation?format=bibtex&doi={}'
             ).format(c['identifier'])
-    dat = urllib2.urlopen(url).read()
+    dat = ''
+    try:
+        dat = urllib2.urlopen(url, timeout=30).read()
+    except socket.timeout, e:
+        print "% Error while fetching from crossref.org. Server says:", e
+        # server did not answer, do not try crossref anymore
+        break
     # replace the bibtex identifier with the correct one
     dat = replace_id(dat, c['entry'])
     # store
@@ -170,7 +202,7 @@ for c in [ c for c in citations if c['type']=='inspire' ]:
     # strip surrounding html
     dat = re.sub(r'<.+?>', r'', dat)
     # check for errors the crude way
-    if re.search(r'@(article|report|inproceedings|phdthesis)', dat)==None:
+    if re.search(r'@(article|report|inproceedings|phdthesis|book)', dat)==None:
         c['error'] = 'inspirehep.net says: ' + dat.replace('\n', '')
     else:
         dat = replace_id(dat, c['entry'])
@@ -180,10 +212,12 @@ for c in [ c for c in citations if c['type']=='inspire' ]:
 # Print
 
 # we postpone error reporting for ease of debugging
-errcount = 0
 for c in citations:
-    if 'error' in c:
-        print '% Error:', c['error']
+    if not 'bib' in c:
+        if 'error' in c:
+            print '% Error:', c['error']
+        else:
+            print '% Error: did not fetch citation', c['entry']
         errcount += 1
     else:
         print '% Fetched citation', c['entry']
@@ -202,3 +236,5 @@ for c in citations:
     if 'bib' in c:
         print c['bib']
         print
+
+sys.exit(errcount>0)
